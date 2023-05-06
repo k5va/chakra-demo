1db445e0d22c5b0e7c742a66d8db14229a270cff
k5va <k5va@yandex.ru>
2023-05-06 12:45:16 +0500

    Adds github pages deploying

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/chakra-demo/',
   resolve: {
     alias: { '@': path.resolve(__dirname, './src') },
   },
