91b22c3ee2edf0e24549d37dadb8c9d46aefbda6
k5va <k5va@yandex.ru>
2023-05-06 12:58:49 +0500

    Updates deployment config data

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/chakra-demo/',
   resolve: {
     alias: { '@': path.resolve(__dirname, './src') },
   },
